2c2896b9e9d1f177cb4debf8a98c84e4ea87bdd2
Shweta <38096594+valkyrie55@users.noreply.github.com>
2019-12-07 12:45:26 +0530

    Update remove_duplicates.c

--- Linked lists/remove_duplicates.c
+++ Linked lists/remove_duplicates.c
@@ -1,80 +1,83 @@
-//c program to implement a singly list list
 #include<stdio.h>
 #include<stdlib.h>
+
 struct node{
-    int data;
-    struct node *next;
-}*head, *temp =NULL;
-void create_nodes(int n);
-void display();
-void remove_duplicate();
-int main(){
-    int n;
-    printf("\nEnter the num. of nodes:");
-    scanf("%d",&n);
-    create_nodes(n);
-    printf("\nList is created");
-    printf("\nBefore removal of duplicates:");
-    display();
-    remove_duplicate();
-    printf("\nAfter removal of duplicates:");
-    display();
-    return 0;
-}
+	int data;
+	struct node *next;
+}*head,*temp = NULL;
+
 void create_nodes(int n){
-    struct node *new;
-    int num,i;
-    head =(struct node*)malloc(sizeof(struct node));
-    if(head == NULL)
-        printf("\nMemory can't be allocated");
-    else{
-        printf("\nEnter data for first node(head):");
-        scanf("%d",&num);
-        head->data = num;
-        head->next = NULL;
-        temp = head;
-    }
-    // now create n nodes and adding to linked list
-    for(i=2;i<=n;i++){
-        new =(struct node*)malloc(sizeof(struct node));
-        if(new == NULL){
-        printf("\nMemory can't be allocated");
-        break;
-    }
-    else{
-        printf("\nEnter data for the node:");
-        scanf("%d",&num);
-        new->data = num;
-        new->next =NULL;
-        temp->next = new;
-        temp = temp->next;
-    }
-    }
+	int num,i;
+	struct node *new;
+	head = (struct node*)malloc(sizeof(struct node));
+	if(head == NULL)
+		printf("\nMemory can't be allocated!");
+	else
+	{
+		printf("\nEnter data for head:");
+		scanf("%d", &num);
+		head->data = num;
+		head->next = NULL;
+		temp = head;
+	}
+	//create remaining nodes
+	for(i = 2; i <= n; i++){
+		new = (struct node*)malloc(sizeof(struct node));
+		if(new == NULL)
+			printf("\nMemory can't be allocated!");
+		else{
+			printf("\nEnter data for new node:");
+			scanf("%d", &num);
+			new->data = num;
+			temp->next = new;
+			temp = temp->next;
+		}
+		
+	}
 }
+
 void display(){
-    struct node *q;
-    if(head ==NULL)
-        printf("\nMemory can't be allocated");
-    else{
-        q = head;
-        while(q!=NULL){         
-        printf("%d->",q->data);
-        q=q->next;
-        }
-    }
+	struct node * q;
+	if(head == NULL)
+		printf("\nEmpty linked list");
+	else{
+		q = head;
+		while(q!=NULL)
+		{
+			printf("%d ->",q->data);
+			q = q->next;
+		}
+		if(q == NULL)
+			printf("NULL\n");
+	}
 }
-void remove_duplicate(){
-    struct node* cur = head;
-    struct node* cur_next = head;
-    if(head == NULL)
-        return;
-    while(cur->next!= NULL){
-        if(cur->data == cur->next->data){
-            cur_next = cur->next->next;
-            free(cur->next);
-            cur->next = cur_next;
-        }
-        else 
-            cur= cur->next;
-    }
+
+
+void remove_duplicates(){
+	struct node *q = head,*cur,*n;
+	while(q->next != NULL){
+		if(q->data == q->next->data)
+			{
+				cur = q->next;
+				n = q->next->next;
+				q->next = n;
+				free(cur);
+			}
+		else
+			q = q->next;
+		}
+	printf("Linked list after removal of duplicates:");
+	display();
+}
+
+int main(){
+	int n, del = 0;
+	printf("\nEnter no. of nodes you want:");
+	scanf("%d", &n);
+	create_nodes(n);
+	printf("\nNodes created!");
+	display();
+	//printf("\nRemoving duplicates from the linked list");
+	remove_duplicates();
+	return 0;
 }
