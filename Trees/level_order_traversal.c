774a2381f44d17623224002c8b71889788c72d39
Shweta <38096594+valkyrie55@users.noreply.github.com>
2019-12-28 14:21:36 +0530

    Update level_order_traversal.c

--- Trees/level_order_traversal.c
+++ Trees/level_order_traversal.c
@@ -1,80 +1,86 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-struct node{
-	int data;
-	struct node* left;
-	struct node* right;
-};
-
-
-/* Function protoypes */
-void printGivenLevel(struct node* root, int level); 
-int height(struct node* node); 
-struct node* newNode(int data); 
-
-/* Function to print level 
-order traversal a tree*/
-void printLevelOrder(struct node* root) 
+#include<stdio.h> 
+#include<stdlib.h> 
+   
+struct node 
 { 
-	int h = height(root); 
-	int i; 
-	for (i = 1; i <= h; i++) 
-		printGivenLevel(root, i); 
+    int data; 
+    struct node *left, *right; 
+}; 
+   
+int height(struct node* node){
+	if(node == NULL)
+		return 0;
+	else{
+		int lh = height(node->left);
+		int rh = height(node->right);
+		if(lh > rh)
+			return (lh+1);
+		else
+			return (rh+1);
+}
+}
+
+void printlevel(struct node* node, int level){
+	if(node == NULL)
+		return;
+	if(level == 1)
+		printf("%d ", node->data);
+	else if(level > 1)
+		printlevel(node->left,level-1);
+	    printlevel(node->right,level-1);
+}
+
+void levelorder(struct node* node){
+	int h = height(node);
+	printf("\nHeight of tree is: %d\n", h);
+	int i;
+	for(i=1;i <= h;i++){
+		printlevel(node, i);
+	}
+}
+
+struct node *createnode(int val) 
+{ 
+    struct node *temp =  (struct node *)malloc(sizeof(struct node)); 
+    temp->data = val; 
+    temp->left = temp->right = NULL; 
+    return temp; 
 } 
 
-/* Print nodes at a given level */
-void printGivenLevel(struct node* root, int level) 
-{ 
-	if (root == NULL) 
-		return; 
-	if (level == 1) 
-		printf("\n%d",root->data); 
-	else if (level > 1) 
-	{ 
-		printGivenLevel(root->left, level-1); 
-		printGivenLevel(root->right, level-1); 
-	} 
-} 
+struct node* insert(struct node* node, int val)
+{
+    if(node == NULL)
+    	return createnode(val);  //root created
+    if(val < node->data)
+        node->left = insert(node->left ,val);
+    else if (val > node->data)
+    	node->right = insert(node->right,val);
+    return node;
+}
 
-int height(struct node* node) 
-{ 
-	if (node == NULL) 
-		return 0; 
-	else
-	{ 
+void preorder(struct node* root){
+	if(root!= NULL){
+		printf("\n%d", root->data);
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
 
-		int lheight = height(node->left); 
-		int rheight = height(node->right); 
-
-		/* use the larger one */
-		if (lheight > rheight) 
-			return(lheight + 1); 
-		else return(rheight + 1); 
-	} 
-} 
-
-struct node* newNode(int data) 
-{ 
-	struct node* Node = (struct node*)malloc(sizeof(struct node));
-	Node->data = data; 
-	Node->left = NULL; 
-	Node->right = NULL; 
-
-	return(Node); 
-} 
-
-/* Driver code*/
-int main() 
-{ 
-	struct node *root = newNode(1); 
-	root->left = newNode(2); 
-	root->right = newNode(3); 
-	root->left->left = newNode(4); 
-	root->left->right = newNode(5); 
-
-	printf("Level Order traversal of binary tree is \n"); 
-	printLevelOrder(root); 
-
-	return 0; 
-} 
+int main()
+{
+	struct node* root = NULL;
+	root = insert(root,50);
+	insert(root, 30);
+	insert(root, 20);
+	insert(root, 40);
+	insert(root, 70);
+	insert(root, 60);
+    insert(root, 80);
+    printf("\n tree created!");
+    preorder(root);
+    printf("\n********************\n");
+    //printf("%d ", root->data);
+    //display(root);
+    levelorder(root);
+    return 0;
+}
