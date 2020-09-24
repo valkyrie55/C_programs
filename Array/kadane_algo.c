e5d48782c295d8d1fe88ee9786256ca093ce3d96
Shweta <38096594+valkyrie55@users.noreply.github.com>
2020-09-24 13:23:47 +0530

    Update kadane_algo.c

--- Array/kadane_algo.c
+++ Array/kadane_algo.c
@@ -1,27 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-void swap(int *a, int *b){
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+int kadane(int arr[], int n){
+    int max_so_far = -99999, max = 0;
+    int i;
+    for(i = 0;i<n;i++){
+        max += arr[i];
+        if(max_so_far < max)
+            max_so_far = max;   //update it
+        if(max < 0)
+            max = 0;
+    }
+    return max_so_far;
 }
 
-int main(){
-	int n, i, max_so_far = 0, max_ending_here = 0;
-	printf("\nEnter array size: ");
-	scanf("%d", &n);
-	int arr[n];
-	printf("\nEnter array elements:");
-	for (i = 0;i < n; i++)
-		scanf(" %d", &arr[i]);
-	for(i = 0; i < n; i++){
-		max_ending_here += arr[i];
-		if(max_so_far < max_ending_here)
-			max_so_far = max_ending_here;
-		if(max_ending_here < 0)
-			max_ending_here = 0;
-	}
-	printf("\nMax. contiguous sum: %d", max_so_far);
-	return 0;
-}
+
+int main() 
+{ 
+    int a[] = {-2, -3, 4, -1, -2, 1, 5, -3}; 
+    int n = sizeof(a)/sizeof(a[0]); 
+    int max_sum = kadane(a, n); 
+    printf("Maximum contiguous sum is: %d", max_sum);
+    return 0; 
+} 
